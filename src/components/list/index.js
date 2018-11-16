a5acc2dfca82c70ffc4ce76360881605031d8a04
Steven Sierra <stevensierra.dev@gmail.com>
2018-11-16 10:15:38 -0500

    improve proptypes

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
 
 import Item from './item';
 
 function List ({services, deleteService}) {
     if(services.length === 0) {
-        return<Typography 
-                align="center" 
-                component="h1" 
-                color="secondary" 
-                variant="display1" 
-                style={{marginTop: 50, fontSize: '10vh'}}
-                gutterBottom
-            >
-                Get start =>
-            </Typography>
+        return<Card style={{height: 350, width: 500, marginLeft: 'auto', marginRight: 'auto'}}>
+            <CardActionArea>
+            <img src="https://upload.wikimedia.org/wikipedia/sr/8/8b/Google_Maps_screenshot.png" alt="map"/>
+            <CardContent>
+                <Typography 
+                    align="center" 
+                    component="h1" 
+                    color="secondary" 
+                    variant="display1" 
+                    style={{marginTop: 0, fontSize: '5vh'}}
+                    gutterBottom
+                >
+                    Get start =>
+                </Typography>
+            </CardContent>
+            </CardActionArea>
+        </Card>
     }
 
     return<Grid container>
@@ -29,6 +40,11 @@ function List ({services, deleteService}) {
             )  
         }
     </Grid>
+};
+
+List.propTypes = {
+    deleteService: PropTypes.func.isRequired,
+    services: PropTypes.array.isRequired,
 };
 
 export default List
